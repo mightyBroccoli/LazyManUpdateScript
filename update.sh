f08941a2bf00ced0191f24880a2db92130706135
nico <nico@magicbroccoli.de>
2016-06-27 17:43:54 +0200

    * changed .gitignore file * changed file integrity + added user decision for each script function * added awk dependencie * reworked all newline parameters * changed .dependecy file location

--- update.sh
+++ update.sh
@@ -4,6 +4,8 @@
 #mb
 
 #variables
+#possibly change the location
+FILE="~/scripts/lazymanupdatescript/.dependencies"
 RED="\033[0;31m"
 GREEN="\033[0;32m"
 YELLOW="\033[1;33m"
@@ -14,24 +16,24 @@ CYAN="\033[1;36m"
 OLDCONF=$(dpkg -l|grep "^rc"|awk '{print $2}')
 
 #some subroutines
+root_check()
+#is the running user root?
+{
+if [ $USER != root ]; then
+  echo -e $RED"Error: must be root \n"
+  echo -e $YELLOW"Exiting...\n"$ENDCOLOR
+  exit 0
+fi
+}
 aptitude_check ()
 #check if package 'aptitude' is installed
 {
   if command -v aptitude > /dev/null; then
     echo -e $GREEN"Detected aptitude"$ENDCOLOR
   else
-    echo $RED"Installing aptitude..."$ENDCOLOR
+    echo $RED"Installing aptitude...\n"$ENDCOLOR
     apt-get install -q -y aptitude
   fi
-}
-root_check()
-#is the running user root?
-{
-if [ $USER != root ]; then
-  echo -e $RED"Error: must be root"
-  echo -e $YELLOW"Exiting..."$ENDCOLOR
-  exit 0
-fi
 }
 dpkg_check ()
 #check if package 'dpkg' is installed
@@ -39,74 +41,142 @@ dpkg_check ()
   if command -v dpkg > /dev/null; then
     echo -e $GREEN"Detected dpkg"$ENDCOLOR
   else
-    echo $RED"Installing dpkg..."$ENDCOLOR
+    echo $RED"Installing dpkg...\n"$ENDCOLOR
     apt-get install -q -y dpkg
   fi
 }
+awk_check ()
+#check if package 'aptitude' is installed
+{
+  if command -v mawk > /dev/null; then
+    echo -e $GREEN"Detected mawk"$ENDCOLOR
+  else
+    echo $RED"Installing awk...\n"$ENDCOLOR
+    apt-get install -q -y mawk
+  fi
+}
+
 
 #running subroutines
-dpkg_check
-aptitude_check
-root_check
+if [ -f "$FILE" ];
+then
+  echo "Dependencies already checked.\n"
+else
+  echo "Checking now /n"
+  root_check
+  dpkg_check
+  aptitude_check
+  awk_check
+  touch /home/nico/scripts/lazymanupdatescript/.dependencies
+fi
+
 
 # --- the actual script ---
 echo ------
 echo ------
 #apt-get update with the option assume yes (-y) just showing new packets
-echo " "
-echo -e $RED "resynchronizing the package index..." $ENDCOLOR
-echo " "
+echo -e $RED "\n resynchronizing the package index...\n" $ENDCOLOR
 apt update -y | grep -E "^Holen|^Get"
 
-echo ------
-echo -e $GREEN "upgrade" $ENDCOLOR 
-echo ------
-#automaticly updating the installed packages from available ressources
-#apt-get upgrade with the option assume yes (-y)
-apt upgrade -y
-#apt-get dist-upgrade with the option assume yes (-y)
-apt dist-upgrade -y
+#showing upgradable packages
+echo -e $GREEN "\n upgradable packages: \n" $ENDCOLOR
+apt list --upgradable
+echo "\n"
 
-echo ------
-echo -e $YELLOW "dependencies" $ENDCOLOR
-echo ------
-#check for broken dependencies
-#apt-get check with the option assume yes (-y)
-apt-get check -y
-#apt-get install with the option fix-broken (-f) and fix-missing (-m) and assume yes (-y)
-apt install -f -m -y
-
-echo ------
-echo -e  $BLUE "cleaning" $ENDCOLOR
-echo ------
-#various cleaning options
-apt-get autoremove -y
-apt-get autoclean -y
-apt-get clean -y
-aptitude autoclean
-
-echo " "
-echo -e $RED "dumping local trash files..." $ENDCOLOR
-echo " "
-rm -rf /home/*/.local/share/Trash/*/** &> /dev/null
-rm -rf /root/.local/share/Trash/*/** &> /dev/null
-#cleaning user/ root temp files
-
-echo ------
-echo -e $PURPLE "Removing old config files" $ENDCOLOR
-echo ------
-#removing unused config files !!NO ASSUME YES HERE TO PREVENT BROKEN STUFF!!
-aptitude purge $OLDCONF
-
-#check if reboot is needed
-echo ------
-echo -e $CYAN "Should I consider a reboot?" $ENDCOLOR
-echo ------
-if [ -f /var/run/reboot-required ]; then
-  echo -e $CYAN 'reboot is required'$ENDCOLOR
-  else
-  echo -e $CYAN 'no reboot required'$ENDCOLOR
+#upgrading packages
+read -p "Upgrade packages? " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  # n
+  echo -e "\n skipping ... \n"
+else
+  # y
+  echo -e "\n"
+  echo ------
+  echo -e $GREEN "upgrade" $ENDCOLOR 
+  echo ------
+  #automaticly updating the installed packages from available ressources
+  #apt-get upgrade with the option assume yes (-y)
+  apt upgrade -y
+  #apt-get dist-upgrade with the option assume yes (-y)
+  apt dist-upgrade -y
 fi
 
+#dependencies
+read -p "Check all dependencies? " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  # n
+  echo -e "\n skipping ... \n"
+else
+  # y
+  echo -e "\n"
+  echo ------
+  echo -e $YELLOW "dependencies" $ENDCOLOR
+  echo ------
+  #check for broken dependencies
+  #apt-get check with the option assume yes (-y)
+  apt-get check -y
+  #apt-get install with the option fix-broken (-f) and fix-missing (-m) and assume yes (-y)
+  apt install -f -m -y
+fi
+
+#cleaning
+echo -e "\n"
+echo "Cleaning involves :"
+echo "* removing unused packages"
+echo "* cleanig out package list cache"
+echo -e "\n"
+read -p "Should cleaning be done? " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  # n
+  echo -e "\n skipping ... \n"
+else
+  # y
+  echo -e "\n"
+  echo ------
+  echo -e  $BLUE "cleaning" $ENDCOLOR
+  echo ------
+  #various cleaning options
+  apt-get autoremove -y
+  apt-get autoclean -y
+  apt-get clean -y
+  aptitude autoclean
+
+  echo " "
+  echo -e $RED "dumping local trash files..." $ENDCOLOR
+  echo " "
+  rm -rf /home/*/.local/share/Trash/*/** &> /dev/null
+  rm -rf /root/.local/share/Trash/*/** &> /dev/null
+  #cleaning user/ root temp files
+fi
+
+#removing old config files and checking if reboot is needed
+read -p "Remove old config files ? " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  # n
+  echo -e "\n skipping ... \n"
+else
+  # y
+  echo -e "\n"
+  echo ------
+  echo -e $PURPLE "Removing old config files" $ENDCOLOR
+  echo ------
+  #removing unused config files !!NO ASSUME YES HERE TO PREVENT BROKEN STUFF!!
+  aptitude purge $OLDCONF
+fi
+
+ #check if reboot is needed
+  echo ------
+  echo -e $CYAN "Should I consider a reboot?" $ENDCOLOR
+  echo ------
+  if [ -f /var/run/reboot-required ]; then
+    echo -e $CYAN 'reboot is required'$ENDCOLOR
+    else
+    echo -e $CYAN 'no reboot required'$ENDCOLOR
+  fi
+
 #clearing variables
-unset RED REDBACK GREEN GREENBACK YELLOW BLUE PURPLE ENDCOLOR CYAN OLDCONF
+unset RED REDBACK GREEN GREENBACK YELLOW BLUE PURPLE ENDCOLOR CYAN OLDCONF FILE
